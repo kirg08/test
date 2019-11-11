1bc0de6d94c00585f67fa8cea086819cbf1ae243
Garanin-KA <kirill.g08@mail.ru>
2019-11-11 10:34:14 +0400

    Добавлено копирование на несколько компьютеров и запись в фаил куда не получилось скопирвать

--- Test1.py
+++ Test1.py
@@ -1,16 +1,21 @@
 import os
 import shutil
-directory = '\\\\10.180.32.1\c$\\Users'
-dir_user = []
-files = os.listdir(directory)
-for dir in files:
-    if "-" in dir:
-        path = directory + "\\" + dir + "\\AppData\\Local\\Lotus\\Notes\\"
-        if os.path.isdir(path):
-            shutil.copyfile(r"test.ini", path + r"test.ini")
-            os.rename(path + r"test.ini", path + r"test_old.ini")
-            print(path)
 
-
-#except:
-    #print("Connection failed")
+for i in range(5):
+    try:
+        directory = '\\\\10.180.32.'+str(i)+'\c$\\Users' #Добавить \\Data
+        dir = []
+        files = os.listdir(directory)
+        for dir in files:
+            if "-" in dir:
+                path = directory + "\\" + dir + "\\AppData\\Local\\Lotus\\Notes\\"
+                if os.path.isdir(path):
+                    shutil.copyfile(r"test.ini", path + r"test.ini")
+                    os.rename(path + r"test.ini", path + r"test_old.ini")
+                    print(path)
+    except:
+        print("Connection failed")
+        file = open("Connection_failed.txt", "w")
+        file.write(directory)
+        file.close()
+        continue
